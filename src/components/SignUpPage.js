0e03f2469e62ace8e1be3f9c8cfb509be2d4ea46
Selaelo1 <selaelolanga10@gmail.com>
2024-10-02 19:30:36 +0200

    create signUpPage

---
+++ src/components/SignUpPage.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import GoogleSignUp from './GoogleSignUp';
+
+
+ const SignUpPage = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('/api/signup', { name, email, password });
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+    return (
+    <div>
+      <h1>Signup</h1>
+      
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Name"
+        />
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
+        <button type="submit">Signup</button>
+      </form>
+      <GoogleSignUp />
+    </div>
+  );
+};
+
+export default SignUpPage;
+
+
+
