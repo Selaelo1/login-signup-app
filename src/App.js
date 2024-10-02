89938dee01f1e0b4ad79c5f1f112fbef28ebb8cf
Selaelo1 <selaelolanga10@gmail.com>
2024-10-02 19:32:58 +0200

    Create App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import WelcomePage from './components/WelcomePage';
+import SignUpPage from './components/SignUpPage';
+import LoginPage from './components/LoginPage';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<WelcomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
+
 export default App;
+
