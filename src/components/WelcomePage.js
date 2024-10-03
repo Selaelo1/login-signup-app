3679de7ef49d8a76b5f51a6009fb0c3eb047c098
Selaelo1 <selaelolanga10@gmail.com>
2024-10-03 21:01:52 +0200

    Update WelcomePage.js

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,30 +1,37 @@
+
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './WelcomePage.css';
-import Logo from '../images/logo512.png'
+import Logo from '../images/logo512.png';
 
 function WelcomePage() {
   return (
     <div className="welcome-page">
-      <div className="container">
-        <h1>Welcome to BodyBliss! </h1>
-        <p>"Your premier destination for fitness, wellness, and community. Explore our classes, trainers, and amenities."</p>
-        <img src={Logo} alt="BodyBliss Logo" className="logo" />
+     
+      <h1>Welcome to BodyBliss!</h1>
+      <p>
+        "Your premier destination for fitness, wellness, and community. Explore our classes, trainers, and amenities."
+      </p>
+       <img src={Logo} alt="BodyBliss Logo" className="logo" />
 
-        <div className="buttons">
-          <Link to="/signup" className="signup-button">Sign Up</Link>
-         
-        </div>
-        <p className="already-have-account">Already have an account? Great!
-        
-        </p>
-        <p>
-          <Link to="/login" className="login-button">Log in then </Link>
-        </p>
-      </div>
+      <Link to="/signup" className="signup-button" aria-label="Sign up for BodyBliss">
+        Sign Up
+      </Link>
+      <p className="already-have-account">
+        Already have an account? Great!
+      </p>
+      <p>
+        <Link to="/login" className="login" aria-label="Log in to BodyBliss">
+          Login 
+          
+        </Link>
+        <span className="no-underline">then!</span>
+      </p>
     </div>
   );
 }
 
 export default WelcomePage;
+
+
 
