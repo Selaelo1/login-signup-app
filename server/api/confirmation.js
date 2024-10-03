c359322b1976cbbe5c7165990ff1508bd12d359d
Selaelo1 <selaelolanga10@gmail.com>
2024-10-03 21:03:21 +0200

    Delete confirmation.js

--- server/api/confirmation.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/confirmation', (req, res) => {
-  res.send('Confirmation page');
-});
-
-module.exports = router;
