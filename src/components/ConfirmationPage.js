301f10ce692c6e5d5526aa8dbc573549fef75aed
Selaelo1 <selaelolanga10@gmail.com>
2024-10-04 16:38:42 +0200

    Update ConfirmationPage.js

--- src/components/ConfirmationPage.js
+++ src/components/ConfirmationPage.js
@@ -2,8 +2,8 @@
 import React, { useEffect } from 'react';
 import './ConfirmationPage.css';
 import { Link } from 'react-router-dom';
-
 import axios from 'axios';
+import Logo4 from '../images/logo192.png';
 
 const ConfirmationPage = () => {
   useEffect(() => {
@@ -16,16 +16,18 @@ const ConfirmationPage = () => {
       });
   }, []);
 
-
   return (
     <div className="confirmation-page">
-      <h1>Thank you for signing up!</h1>
-      <p>We're excited to have you join the BodyBliss community.</p>
-      <p>A confirmation email has been sent to your email address.</p>
-      <button className="login-button">
-        <Link to="/login">Log In</Link>
-      </button>
-
+      <div className="container">
+        <img src={Logo4} className="logo4" />
+        <h1>Thank you for signing up!</h1>
+        <p>We're excited to have you join the BodyBliss community.</p>
+        <p>A confirmation email has been sent to your email address.</p>
+        <button className="login-button">
+          <Link to="/login">Log In</Link>
+        </button>
+        <p>Already have an account? </p>
+      </div>
     </div>
   );
 };
