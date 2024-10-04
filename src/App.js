a836ba9ad39ac7147197b49be50de7ee0632126d
Selaelo1 <selaelolanga10@gmail.com>
2024-10-04 16:38:05 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import WelcomePage from './components/WelcomePage';
 import SignUpPage from './components/SignUpPage';
 import LoginPage from './components/LoginPage';
 import ConfirmationPage from './components/ConfirmationPage';
+import DashboardPage from './components/DashboardPage';
 
 
 function App() {
@@ -15,6 +16,8 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/confirmation" element={<ConfirmationPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+
       </Routes>
     </BrowserRouter>
   );
