91cb134fb5a23d1f1f65655539425a709cb72de0
Selaelo1 <selaelolanga10@gmail.com>
2024-10-04 16:38:09 +0200

    Create client.js

---
+++ src/api/client.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://localhost:3000/api',
+});
+
+export const login = async (email, password) => {
+  try {
+    const response = await api.post('/login', { email, password });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const signup = async (userData) => {
+  try {
+    const response = await api.post('/signup', userData);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
